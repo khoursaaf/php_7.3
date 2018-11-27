8141b9013500f1d86dbfa0dc6ca01daf14b34652
khoursaaf <khoursa@gmail.com>
2018-11-27 15:21:23 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="user.php" method="get">
+    	<p>
+    		<input type="text" name="lastname" placeholder="Votre Nom">
+    		<input type="text" name="firstname" placeholder="Votre Prenom">
+    	</p>
+    	<input type="submit" value="Valider">
+    </form>
+  </body>
+</html>
+
---
+++ user.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User</title>
+</head>
+<body>
+	<p><?php echo $_GET['lastname']; ?></p>
+	<p><?php echo $_GET['firstname']; ?></p>
+</body>
+</html>
